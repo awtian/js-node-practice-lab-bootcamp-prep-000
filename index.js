1f135a5afe530053955d0cde71a11aa6150ee933
Awtian Akbar <me@awtian.com>
2017-10-27 01:01:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // write your code below!
 
 var name = "Awtian"
-var height = "74"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(height)
+console.log((height)+1)
 
-console.log(parseInt(height)+1)
+
